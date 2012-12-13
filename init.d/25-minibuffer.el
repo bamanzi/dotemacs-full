1df98c8ec7197620c5b89c8dad315363e6995294
bamanzi <none@none>
2012-12-13 22:14:17 +0800

    allow ido-mode for files

--- init.d/25-minibuffer.el
+++ init.d/25-minibuffer.el
@@ -21,8 +21,8 @@
 (setq ido-enable-flex-matching t)
 (setq ido-use-filename-at-point 'guess)
 (setq ido-use-url-at-point 'guess)
-;;(ido-mode t)
-(ido-mode 'buffers)
+(setq ido-auto-merge-work-directories-length -1) ;;don't auto jump between directories
+(ido-mode t)
 
 
 ;;** smex : ido for M-x
