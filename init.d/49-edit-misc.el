c0ace68412b448a98e70f82d1d846ec1a5797e7d
bamanzi <none@none>
2013-02-19 22:32:13 +0800

    add a better goto-line

--- init.d/49-edit-misc.el
+++ init.d/49-edit-misc.el
@@ -3,6 +3,19 @@
 ;;*** linum-mode: emacs built-in 
 ;; linum-mode
 
+;;a better goto-line
+;;stolen from http://whattheemacsd.com//key-bindings.el-01.html
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
+
 ;;*** setnu.el
 (autoload 'setnu-mode "setnu" "Toggle setnu-mode." t)
 
